499a49eb29be75f582b4282aaf718a91192f4e23
Ryan <ryan_tso@hotmail.com>
2021-06-11 21:46:50 -0700

    Temp NavBar with drawers, need to fix styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 import AuthScreen from './screens/AuthScreen';
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
   return <ThemeProvider theme={theme}>
+    <NavBar />
     <AuthScreen />
   </ThemeProvider>;
 };
